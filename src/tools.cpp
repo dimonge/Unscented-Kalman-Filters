6e77439c9dee19c0501d20f199900bc01eb72b9c
Peter Shodeinde <petershodeinde4@gmail.com>
2018-01-01 22:42:56 +0200

    implemented the unscented kalman filter algorithm to detect moving items in simulator

--- src/tools.cpp
+++ src/tools.cpp
@@ -15,4 +15,27 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+  VectorXd rmse(4);
+  rmse << 0, 0, 0, 0;
+  int estimations_size = estimations.size();
+  int ground_truth_size = ground_truth.size();
+  if (estimations_size != ground_truth_size || estimations_size == 0) {
+    cout << "Invalid estimations and ground_truth" << endl;
+    return rmse;
+  }
+
+  for (unsigned int i = 0; i < estimations_size; ++i) {
+    VectorXd residual = estimations[i] - ground_truth[i];
+
+    residual = residual.array() * residual.array();
+    rmse += residual;    
+  }
+  // Calculate mean
+  rmse = rmse / estimations_size;
+  // SQRT 
+  rmse = rmse.array().sqrt();
+
+  // return error
+  cout << "RMSE: " << rmse << endl;
+  return rmse;
 }
